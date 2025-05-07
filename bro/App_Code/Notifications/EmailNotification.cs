b687ad7248d38b9af12712d9f073dcf57e675990
Senzo <senzosigh@gmail.com>
2025-05-07 13:41:26 +0200

    App code folder for Interface and classes

---
+++ bro/App_Code/Notifications/EmailNotification.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace bro.App_Code.Notifications
+{
+    public class EmailNotification
+    {
+    }
+}
