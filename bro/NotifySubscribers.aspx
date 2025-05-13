ec03370f58a6619c9a0fba179781d951307ac8f9
Senzo <senzosigh@gmail.com>
2025-05-13 15:17:55 +0200

    Email Subscribers receive notification

---
+++ bro/NotifySubscribers.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="NotifySubscribers.aspx.cs" Inherits="bro.NotifySubscribers" %>
+
+<!DOCTYPE html>
+<html>
+<head runat="server">
+    <title>Send Notifications</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="padding:20px;">
+            <h2>Send Notification to Subscribers</h2>
+            <asp:TextBox ID="txtMessage" runat="server" TextMode="MultiLine" Rows="6" Columns="60" placeholder="Enter your notification message here..." />
+            <br /><br />
+            <asp:Button ID="btnSend" runat="server" Text="Send to Email Subscribers" OnClick="btnSend_Click" />
+            <br /><br />
+            <asp:Label ID="lblStatus" runat="server" ForeColor="Green" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ bro/NotifySubscribers.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net.Mail;
+using System.Net;
+using bro.Models;
+using Notifications;
+using System.Linq;
+
+
+namespace bro
+{
+    public partial class NotifySubscribers : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void btnSend_Click(object sender, EventArgs e)
+        {
+            string message = txtMessage.Text;
+
+            try
+            {
+                using (var db = new GamePulseDBContext())
+                {
+                    var emailSubscribers = db.Subscriptions
+                                             .Where(s => s.NotifyByEmail)
+                                             .ToList();
+
+                    foreach (var subscriber in emailSubscribers)
+                    {
+                        MailMessage mail = new MailMessage();
+                        mail.To.Add(subscriber.Email);
+                        mail.From = new MailAddress("siyabongmelissa@gmail.com");
+                        mail.Subject = "Game Update from SZS Game Pulse";
+                        mail.Body = message;
+                        mail.IsBodyHtml = true;
+
+                        SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+                        smtp.Credentials = new NetworkCredential("siyabongmelissa@gmail.com", "iwswiecdznirbubq");
+                        smtp.EnableSsl = true;
+
+                        smtp.Send(mail);
+                    }
+
+                    lblStatus.Text = "Emails successfully sent to all email subscribers.";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = "Error: " + ex.Message;
+                lblStatus.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+    }
+}
